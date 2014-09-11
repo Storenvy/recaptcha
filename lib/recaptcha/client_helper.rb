646d443da8b572b5c6bd31f34bf0275037b5c6e0
Jon Crawford <jon@storenvy.com>
2014-09-10 20:34:22 -0700

    Includes widget HTML directly in the view for custom styling

--- lib/recaptcha/client_helper.rb
+++ lib/recaptcha/client_helper.rb
@@ -38,6 +38,27 @@ module Recaptcha
           </script>
         EOS
       else
+        if options[:display] && options[:display][:custom_theme_widget]
+          html << <<-EOS
+           <div id="recaptcha_widget" style="display:none">
+
+             <div id="recaptcha_image"></div>
+             <div class="recaptcha_only_if_incorrect_sol" style="color:red">Incorrect please try again</div>
+
+             <span class="recaptcha_only_if_image">Enter the words above:</span>
+             <span class="recaptcha_only_if_audio">Enter the numbers you hear:</span>
+
+             <input type="text" id="recaptcha_response_field" name="recaptcha_response_field" />
+
+             <div><a href="javascript:Recaptcha.reload()">Get another CAPTCHA</a></div>
+             <div class="recaptcha_only_if_image"><a href="javascript:Recaptcha.switch_type('audio')">Get an audio CAPTCHA</a></div>
+             <div class="recaptcha_only_if_audio"><a href="javascript:Recaptcha.switch_type('image')">Get an image CAPTCHA</a></div>
+
+             <div><a href="javascript:Recaptcha.showhelp()">Help</a></div>
+
+           </div>
+          EOS
+        end
         html << %{<script type="text/javascript" src="#{uri}/challenge?k=#{key}}
         html << %{#{error ? "&amp;error=#{CGI::escape(error)}" : ""}}
         html << %{#{lang ? "&amp;lang=#{lang}" : ""}"></script>\n}
